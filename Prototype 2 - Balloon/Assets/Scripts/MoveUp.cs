81600d102798d5dab3bc79c9db51370454cc4164
deanp105 <dean.p105@stu.nebo.edu>
2025-09-23 09:15:16 -0600

    added despawner, debugged

--- Prototype 2 - Balloon/Assets/Scripts/MoveUp.cs
+++ Prototype 2 - Balloon/Assets/Scripts/MoveUp.cs
@@ -5,18 +5,27 @@ public class MoveUp : MonoBehaviour
 
 
     public float speed = 3.0f;
-
+    public float upperBound = 15.0f;
+    public ScoreManager scoreManager; //reference to score manager
+    public Balloon balloon; //reference to balloon script
 
     void Start()
     {
-    
-}
+        scoreManager = GameObject.Find("ScoreManager").GetComponent<ScoreManager>();
+        balloon = GetComponent<Balloon>();
+    }
 
 
     void Update()
     {
         transform.Translate(Vector3.up * speed * Time.deltaTime); // move balloon up on y axis
-}
+
+        if (transform.position.y >= upperBound)
+        {
+            scoreManager.DecreaseScoreText(balloon.scoreToGive);
+            Destroy(gameObject);
+        }
+    }
 
 
 
