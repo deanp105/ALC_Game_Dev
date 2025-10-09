dbf0de1704b3b042ec1b5bd7056d3e6dc7c04ea4
deanp105 <dean.p105@stu.nebo.edu>
2025-10-09 09:22:40 -0600

    created player controller

---
+++ Prototype - 3 - 2DPlatformer/Assets/Scripts/PlayerController2D.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class PlayerController2D : MonoBehaviour
+{
+    [Header("Player Settings")]
+    public float moveSpeed;
+    public float jumpForce;
+    public bool isGrounded;
+    public int bottomBound = -4;
+    [Header("Score")]
+    public int score;
+
+    public Rigidbody2D rig;
+    public TextMeshProUGUI scoreText;
+
+    public void AddScore (int amount)
+{
+    score += amount;
+    scoreText.text = "Score: " + score;
+}
+
+
+void FixedUpdate ()
+{
+    float moveInput = Input.GetAxisRaw("Horizontal");
+    rig.velocity = new Vector2(moveInput * moveSpeed, rig.velocity.y);
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.UpArrow) && isGrounded)
+        {
+            isGrounded = false;
+            rig.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+
+        if(transform.position.y < bottomBound)
+        {
+            GameOver();
+        }
+    }
+
+    void OnCollisionEnter2D (Collision2D collision)
+    {
+        if(collision.GetContact(0).normal == Vector2.up)
+        {
+            isGrounded= true;
+        }
+    }
+
+    public void GameOver()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+}
