4c1bd4dd9514a9e01ce7702d787b15990043bf82
deanp105 <dean.p105@stu.nebo.edu>
2025-10-13 09:06:15 -0600

    coded enemy

---
+++ Prototype - 3 - 2DPlatformer/Assets/Scripts/Enemy.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Enemy : MonoBehaviour
+{
+
+    [Header("Enemy Stats")]
+
+    public float moveSpeed;//enemy speed
+    public Vector3 moveOffset;//enemy direction
+    private Vector3 startPos;//start position
+    private Vector3 targetPos;//target position
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        startPos = transform.position;
+        targetPos = startPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed, Time.deltaTime);
+
+        if (transform.position == targetPos)
+        {
+            targetPos = startPos + moveOffset;
+        }
+        else
+        {
+            targetPos = startPos;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            collision.GetComponent<PlayerController2D>().GameOver();
+        }
+    }
+
+private void OnDrawGizmos()
+    {
+        Vector3 from;
+        Vector3 to;
+
+        if (Application.isPlaying)
+        {
+            from = startPos;
+        }
+        else
+        {
+            from = transform.position;
+        }
+
+        to = from + moveOffset;
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(from, to);
+        Gizmos.DrawWireSphere(to, 0.2f);
+        Gizmos.DrawWireSphere(from, 0.2f);
+    }
+
+
+
+
+
+}
+
+
+
+
